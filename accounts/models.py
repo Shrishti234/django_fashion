7009b4bc815e074a324d990b56010e885a326c83
shrishti <yadavpiyush0310@gmail.com>
2024-04-12 10:48:22 +0530

    django_fashion

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#Create your models here.
-class Profile(models.Model)
+class Profile(models.Model):
     gender_choices = [
-        ('male', "male")
-        ('female', "female")
-        ('other', "Don't want to disclose")
+        ('male', "Male"),
+        ('female', "Female"),
+        ('other', "Dont want to disclose")
     ]
-     first_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    picture = models.ImageField(upload_to='profile/')
-    gender_choices
+    picture= models.ImageField(upload_to='profile/')
+    gender = models.CharField(max_length=10, choices=gender_choices)
+    bio = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
